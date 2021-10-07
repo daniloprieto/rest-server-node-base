072fc704e9b12fad9c5dc5c218964366e44a4c42
Danilo Prieto <danilo.s.prieto@gmail.com>
2021-10-07 10:56:50 -0300

    solved changes

--- models/server.js
+++ models/server.js
@@ -4,39 +4,43 @@ const cors = require('cors');
 class Server {
 
     constructor() {
-        this.app = express();
+        this.app  = express();
         this.port = process.env.PORT;
-        this.userPath = 'api/users';
+        this.usuariosPath = '/api/usuarios';
 
-        //Middlewares
+        // Middlewares
         this.middlewares();
 
-        //Routes
+        // Rutas de mi aplicación
         this.routes();
     }
 
     middlewares() {
-        
-        //cors
-        this.app.use(cors());
 
-        //Read and parse body
-        this.app.use(express.json());
+        // CORS
+        this.app.use( cors() );
 
-        //public
-        this.app.use(express.static('public'));
-    };
+        // Lectura y parseo del body
+        this.app.use( express.json() );
+
+        // Directorio Público
+        this.app.use( express.static('public') );
+
+    }
 
     routes() {
-        this.app.use(this.userPath, require('../routes/users'));
-    };
+        this.app.use( this.usuariosPath, require('../routes/usuarios'));
+    }
 
     listen() {
-        this.app.listen(this.port, () => {
-            console.log('servidor corriendo en puerto: ', this.port)
-        })
-    };
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
 }
 
-module.exports = Server;
 
+
+
+module.exports = Server;
